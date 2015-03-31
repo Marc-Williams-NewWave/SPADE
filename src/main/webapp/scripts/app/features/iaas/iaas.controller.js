4fc9dfdc1f981ca4417e4e001173ade9b041dedd
Marc-Williams-NewWave <marc.williams@newwave-technologies.com>
2015-03-31 08:47:04 -0400

    removed dummy link from navbar and updates kube.min.css to stop effecting html elements unintentionally

--- src/main/webapp/scripts/app/features/iaas/iaas.controller.js
+++ src/main/webapp/scripts/app/features/iaas/iaas.controller.js
@@ -596,23 +596,10 @@ angular.module('spadeApp')
         
      	 $scope.launch = function(){
      		console.log($scope.payload);
-//     		 for(x in $scope.payload.controllers){
-//     			 console.log(x);
-//     		 }
-     		              
-//     		 for(var x = 0; x < $scope.podArray.length; x++){
-//     			 if($scope.podArray[x].replicas > 0){
-     				 //call validation function with current pod
-     				
      		 	$http.post("http://192.168.4.8:8080/spade/api/demo/stacks", $scope.payload)
          		 	.success(function(data){
          		 		console.log("success data returned ====> " + data);
          		 });	
-     				
-     				
-//     			 }
-//     		 }
-     		 
       	}
      	 
 //     	$scope.deletePod = function(pod){
